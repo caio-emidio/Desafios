ab7699c8e79b6efc4ce4a86eb449e094de45f1a2
Caio Emidio de Andrade Carnelós <caio.emidio@gmail.com>
2018-01-27 00:54:22 -0200

    Update ListDownloader.py
    Included GUI Interface :)

--- ListDownloader.py
+++ ListDownloader.py
@@ -2,14 +2,19 @@
 #Author: Caio Emidio de Andrade Carnelos
 #Subject: Download a SimpleList :)
 '''
+
 import urllib.request, urllib.parse, urllib.error
 import sys
+from tkinter import filedialog
+from tkinter import *
 
 try:
 	file = open(sys.argv[1], "r")
 except FileNotFoundError:
-	f = input("File Name: ")
-	file = open(f, "r")
+	#If FileNotFoundError, Open a window with Tkinter only to find in GUI mode :)
+	root = Tk()
+	root.filename =  filedialog.askopenfilename(title = "Select file",filetypes = (("Text Files","*.txt"),("all files","*.*")))
+	file = open(root.filename, "r")
 
 listFiles = file.readlines()
 
